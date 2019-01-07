65dda3e631033b4c5e42dfdcee5eaf5f45aae622
Chenguanghui <13035809409@163.com>
2019-01-07 19:37:55 +0800

    FIX

--- plugins/axios.js
+++ plugins/axios.js
@@ -7,14 +7,5 @@ export default axios.create({
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     },
     'X-Requested-With': 'XMLHttpRequest'
-  },
-  // 超时设置
-  timeout: 10000,
-  // 跨域是否带Token
-  withCredentials: true,
-  // 响应的数据格式 json / blob /document /arraybuffer / text / stream
-  responseType: 'json',
-  // XSRF 设置
-  xsrfCookieName: 'XSRF-TOKEN',
-  xsrfHeaderName: 'X-XSRF-TOKEN',
+  }
 })
