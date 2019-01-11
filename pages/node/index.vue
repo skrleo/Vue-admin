229fc6925b95e8bb5c2445bd37cdf419efd9b5d3
Chenguanghui <13035809409@163.com>
2019-01-11 23:15:52 +0800

    添加节点

--- pages/node/index.vue
+++ pages/node/index.vue
@@ -7,15 +7,39 @@
         </el-breadcrumb>
         <br>
         <el-dialog
-            title="提示"
+            title="添加节点"
             :visible.sync="dialogVisible"
-            width="30%"
-            :before-close="handleClose">
-        <span>这是一段信息</span>
-        <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-        </span>
+            width="35%">
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="节点名称">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="是否启用">
+                <el-switch v-model="form.delivery"></el-switch>
+            </el-form-item>
+            <el-form-item label="路由地址">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="菜单类型">
+                <el-radio-group v-model="form.resource">
+                <el-radio label="一级菜单"></el-radio>
+                <el-radio label="二级菜单"></el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="节点类型">
+                <el-select v-model="form.region" placeholder="请选择父级节点">
+                <el-option label="权限管理" value="beijing"></el-option>
+                <el-option label="站点管理" value="shanghai"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="节点描述">
+                <el-input type="textarea" v-model="form.desc"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
         </el-dialog>
 
         <el-tree
@@ -72,20 +96,22 @@
         }]
       }];
       return {
-          
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
         dialogVisible: false,
         data: JSON.parse(JSON.stringify(data))
       }
     },
 
     methods: {
-        handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => {});
-      },
       append(data) {
         const newChild = { id: id++, label: 'testtest', children: [] };
         if (!data.children) {
