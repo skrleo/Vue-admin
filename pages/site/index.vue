0bfa30fe0d937902b0675b46388804a31cfe92dd
Chenguanghui <13035809409@163.com>
2019-01-20 11:40:13 +0800

    siteConfig

--- pages/site/index.vue
+++ pages/site/index.vue
@@ -6,16 +6,16 @@
       <el-breadcrumb-item>站点设置</el-breadcrumb-item>
     </el-breadcrumb>
     <br>
-    <el-form ref="form" :model="form" label-width="120px">
+    <el-form ref="form" :model="siteConfig" label-width="120px">
     <el-form-item label="网站名称" style="width:400px" >
-      <el-input v-model="form.name"></el-input>
+      <el-input v-model="siteConfig.title"></el-input>
     </el-form-item>
     <el-form-item label="ICP备案号" style="width:400px" >
-       <el-input v-model="form.name"></el-input>
+       <el-input v-model="siteConfig.icpBeian"></el-input>
     </el-form-item>
     <el-form-item label="网站状态" style="width:400px" >
        <el-switch
-        v-model="value3"
+        v-model="status"
         active-text="启用"
         inactive-text="关闭">
         </el-switch>
@@ -28,7 +28,7 @@
           :on-remove="handleRemove"
           :before-remove="beforeRemove"
           multiple
-          :limit="3"
+          :limit="1"
           :on-exceed="handleExceed"
           :file-list="fileList">
           <el-button size="small" type="primary">点击上传</el-button>
@@ -36,10 +36,10 @@
         </el-upload>
     </el-form-item>
     <el-form-item label="SEO关键字" style="width:400px" >
-       <el-input v-model="form.name"></el-input>
+       <el-input v-model="siteConfig.keywords"></el-input>
     </el-form-item>
     <el-form-item label="网站描述" style="width:600px" >
-        <el-input type="textarea" v-model="form.desc" ></el-input>
+        <el-input type="textarea" v-model="siteConfig.description" ></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="onSubmit">立即修改</el-button>
@@ -49,26 +49,45 @@
 </div>
 </template>
 <script>
+  import qs from 'qs';
+  import axios from '~/plugins/axios.js';
   export default {
     layout: 'main',
+    name:'site',
+    async asyncData () {
+      let { data } = await axios.get('site/1')
+      console.log(data.lists);
+      return {
+        siteConfig: data.data
+      }
+    },
     data() {
       return {
-        value3: true,
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
+        status:true
       }
     },
     methods: {
       onSubmit() {
-        console.log('submit!');
+        axios.put('site/1',qs.stringify({
+            logo: 123,
+            cover: 123,
+            status: 1,
+            title: this.siteConfig.title,
+            icpBeian: this.siteConfig.icpBeian,
+            keywords: this.siteConfig.keywords,
+            description: this.siteConfig.description
+          })).then(res => {
+            //判断是否请求成功
+            if(res.data.errorId === 'OK'){
+              this.$message({
+                  message: '成功修改站点配置',
+                  type: 'success'
+                });  
+              this.dialogVisible = false;   
+            }
+          }).catch(res => {
+            this.$message.error('请求错误，请重试');
+          });
       }
     }
   }
