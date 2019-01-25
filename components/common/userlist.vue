d4f1b4f1ce1e504495a1454652ed65bacf597503
Chenguanghui <13035809409@163.com>
2019-01-25 22:58:43 +0800

    fix

--- components/common/userlist.vue
+++ components/common/userlist.vue
@@ -1,64 +1,52 @@
+
 <template>
-  <div>
-    <el-table :data="lists" border style="width: 100%">
-      <!--勾选框-->
-      <el-table-column type="selection" width="55">
-      </el-table-column>
-      <el-table-column prop="uid" label="用户ID" width="120">
-      </el-table-column>
-      <el-table-column prop="name" label="用户账号" width="180">
-      </el-table-column>
-      <el-table-column prop="name" label="姓名" width="180">
-      </el-table-column>
-      <el-table-column prop="phone" label="联系方式">
-      </el-table-column>
-      <el-table-column prop="createdAt" label="创建时间">
-            <template slot-scope="scope">
-              <span>{{scope.row.createdAt | d('yyyy-MM-dd hh:mm:ss')}}</span>
-          </template>
-      </el-table-column>
-    </el-table>
-     <!--分页条-->
-     <br>
-    <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="pageNow"
-        :page-sizes="[10, 50, 100, 150]"
-        :page-size="pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="pageCount">
-    </el-pagination>
-  </div>
+  <section>
+    <el-dialog
+        title="添加角色"
+        :visible.sync="dialogVisible"
+        width="65%">
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+    </el-dialog>
+  </section>
 </template>
 
 <script>
   export default {
-    async asyncData () {
-      let { data } = await axios.get('/user/lists')
-      console.log(data);
+    props: [ 'dialogVisible', 'refresh', 'users', 'siteId'],
+    data () {
       return {
-        pageNow: data.page.now || 1 ,
-        pageSize: data.page.size || 10 ,
-        pageCount: data.page.count || 0 ,
-        lists: data.lists || []
+        name: '', // 用户名搜索
+        phone: '', // 手机搜索
+        pageNow: 1, // 当前页
+        pageSize: 5, // 显示页数
+        count: 0, // 总条数
+        multipleSelection: [],
+        lists: [],
+        userName: []
       }
     },
     methods: {
-       handleSizeChange(val) {
-        axios.get(`/user/lists?pageSize=${val}`)
-        .then(res => {
-            this.lists = res.data.lists || [];
-            this.pageSize = res.data.page.size || 10;
-          });
+      // 关闭之前执行
+      handleClose (done) {
+        
       },
-      handleCurrentChange(val) {
-        axios.get(`/user/lists?pageNow=${val}`)
-        .then(res => {
-            this.lists = res.data.lists || [];
-            this.pageNow = res.data.page.now || 1;
-          });
+      // 选择事件
+      handleSelectionChange (row) {
+        
+      },
+      // 确认
+      affirmSelect () {
+        
+      },
+      getList (page) {
+        
       }
+    },
+    mounted () {
+      // this.getList()
     }
   }
 </script>
--- pages/power/store/index.vue
+++ pages/power/store/index.vue
@@ -1,59 +1,54 @@
 <template>
-  <div>
-    <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/main' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item :to="{ path: '/power' }">权限管理</el-breadcrumb-item>
-      <el-breadcrumb-item>添加权限</el-breadcrumb-item>
-    </el-breadcrumb>
-    <br>
-    <el-form ref="form" :model="form" label-width="80px">
-    <el-form-item label="用户名称" style="width:400px" >
-      <el-button type="primary" size="medium" @click="dialogVisible = true">选择用户</el-button>
-    </el-form-item>
-    <el-form-item label="是否可用">
-      <el-switch v-model="form.delivery"></el-switch>
-    </el-form-item>
-    <el-form-item label="用户角色">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox v-for="item in role" :key="item.roleId" :label="item.name" :value="item.roleId"></el-checkbox>
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="备注" style="width:600px">
-      <el-input type="textarea" v-model="form.desc" ></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">立即创建</el-button>
-      <el-button>取消</el-button>
-    </el-form-item>
-  </el-form>
-  <el-dialog
-      title="添加角色"
-      :visible.sync="dialogVisible"
-      width="65%">
-      <el-user-list></el-user-list>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-      </span>
-  </el-dialog>
-</div>
+  <section>
+    <el-row class="panel panel-flat">
+      <el-row class="panel-body clearfix">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/main' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: '/power' }">权限管理</el-breadcrumb-item>
+          <el-breadcrumb-item>添加权限</el-breadcrumb-item>
+        </el-breadcrumb>
+        <br>
+        <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="用户名称" style="width:400px" >
+          <el-button type="primary" size="medium" @click="chooseUsers">选择用户</el-button>
+        </el-form-item>
+        <el-form-item label="是否可用">
+          <el-switch v-model="form.delivery"></el-switch>
+        </el-form-item>
+        <el-form-item label="用户角色">
+          <el-checkbox-group v-model="form.type">
+            <el-checkbox v-for="item in role" :key="item.roleId" :label="item.name" :value="item.roleId"></el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="备注" style="width:600px">
+          <el-input type="textarea" v-model="form.desc" ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-row>
+  </el-row>
+    <!-- 添加管理员组件 -->
+    <el-user-list :dialogVisible.sync="dialogVisible"></el-user-list>
+  </section>
 </template>
 <script>
   import qs from 'qs';
   import axios from '~/plugins/axios.js';
   import userlist from '~/components/common/userlist.vue'
+  
   export default {
     layout: 'main',
     components: {
-      'el-user-list':userlist
+       'el-user-list':userlist
     },
     async asyncData () {
       let {data} = await axios.get('/rbac/role/lists');
-      // let department = await axios.get('/rbac/node/lists')
       console.log(data.lists);
       return {
         role: data.lists,
-        // department: department.lists
       }
     },
     data() {
@@ -81,6 +76,10 @@
       },
       onSubmit() {
         console.log('submit!');
+      },
+      chooseUsers(){
+        this.dialogVisible = true;
+        console.log('chooseUsers!');
       }
     }
   }
