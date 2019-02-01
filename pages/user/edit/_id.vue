191668058548ff8be34fe4b0b1130aafc8c0ca50
Chenguanghui <13035809409@163.com>
2019-02-01 15:39:00 +0800

    fix

--- pages/user/edit/_id.vue
+++ pages/user/edit/_id.vue
@@ -27,9 +27,9 @@
     </el-form-item>
     <el-form-item label="性别">
       <el-radio-group v-model="form.sex">
-        <el-radio label="男"></el-radio>
-        <el-radio label="女"></el-radio>
-        <el-radio label="保密"></el-radio>
+        <el-radio label="0">男</el-radio>
+        <el-radio label="1">女</el-radio>
+        <el-radio label="2">保密</el-radio>
       </el-radio-group>
     </el-form-item>
     <!-- <el-form-item label="账号密码">
