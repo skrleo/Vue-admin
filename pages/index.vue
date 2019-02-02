53c6ae3f886f96c4706d982dac9dfe257300f269
Chenguanghui <13035809409@163.com>
2019-02-02 23:07:08 +0800

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
         <el-col :span="8">
           <el-card class="box-card" shadow="hover">
             <div slot="header" class="clearfix">
-              <span>代办事件</span>
+              <span>待办事件</span>
               <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
             </div>
             <div v-for="o in 4" :key="o" class="text item">
@@ -52,7 +52,9 @@
               <span>日均PV量</span>
               <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
             </div>
-            <ve-line :data="chartData"></ve-line>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{'列表内容 ' + o }}
+            </div>
           </el-card>
         </el-col>
       </el-row>
