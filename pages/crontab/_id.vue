92f2dff92aefdf45667d1eeb758ea37d3e0198fb
Chenguanghui <13035809409@163.com>
2019-02-03 15:01:36 +0800

    fix

--- pages/crontab/_id.vue
+++ pages/crontab/_id.vue
@@ -4,19 +4,52 @@
       <el-row class="panel-body clearfix">
         <el-breadcrumb separator-class="el-icon-arrow-right">
           <el-breadcrumb-item :to="{ path: '/main' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>角色管理</el-breadcrumb-item>
-          <el-breadcrumb-item>添加角色</el-breadcrumb-item>
+          <el-breadcrumb-item>任务管理</el-breadcrumb-item>
+          <el-breadcrumb-item>添加任务</el-breadcrumb-item>
         </el-breadcrumb>
         <br>
         <el-form ref="role" :model="role" label-width="80px" size="medium">
-          <el-form-item label="角色名称" prop="name">
+          <el-form-item label="任务名称" prop="name">
             <el-input v-model="role.name" style="width:280px;"></el-input>
           </el-form-item>
-          <el-form-item label="角色状态" prop="state">
+          <el-form-item label="类型" prop="state">
              <el-radio-group v-model="role.state">
-              <el-radio label="0">启用</el-radio>
-              <el-radio label="1">禁用</el-radio>
+              <el-radio label="0">执行一次</el-radio>
+              <el-radio label="1">循环执行</el-radio>
             </el-radio-group>
+          </el-form-item>
+          <el-form-item label="时间间隔">
+                <el-input placeholder="请输入间隔时间" v-model="input" class="input-with-select" style="width:280px;">
+                    <el-select v-model="select" slot="append" placeholder="秒">
+                    <el-option label="秒" value="1"></el-option>
+                    <el-option label="分" value="2"></el-option>
+                    <el-option label="时" value="3"></el-option>
+                    </el-select>
+                </el-input>
+          </el-form-item>
+          <el-form-item label="起止时间" prop="state">
+             <el-time-select
+                placeholder="起始时间"
+                v-model="startTime"
+                :picker-options="{
+                start: '08:30',
+                step: '00:15',
+                end: '18:30'
+                }">
+            </el-time-select>
+            <el-time-select
+                placeholder="结束时间"
+                v-model="endTime"
+                :picker-options="{
+                start: '08:30',
+                step: '00:15',
+                end: '18:30',
+                minTime: startTime
+                }">
+            </el-time-select>
+          </el-form-item>
+          <el-form-item label="执行任务" style="width:600px" prop="description">
+            <el-input v-model="role.name" style="width:280px;"></el-input>
           </el-form-item>
           <el-form-item label="备注" style="width:600px" prop="description">
             <el-input type="textarea" v-model="role.description"></el-input>
@@ -42,6 +75,9 @@
           state:'',
           description:''
         },
+        startTime: '',
+        endTime: '',
+        select: '',
         dialogVisible: false
       }
     },
@@ -70,3 +106,11 @@
     }
   }
 </script>
+<style>
+  .el-select .el-input {
+    width: 68px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+</style>
