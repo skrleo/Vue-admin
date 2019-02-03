9214ec7f4eaf3ead1286ca9053cf092ee2a59387
Chenguanghui <13035809409@163.com>
2019-02-03 20:41:55 +0800

    fix

--- pages/crontab/_id.vue
+++ pages/crontab/_id.vue
@@ -5,21 +5,21 @@
         <el-breadcrumb separator-class="el-icon-arrow-right">
           <el-breadcrumb-item :to="{ path: '/main' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>任务管理</el-breadcrumb-item>
-          <el-breadcrumb-item>任务详情</el-breadcrumb-item>
+          <el-breadcrumb-item>编辑任务</el-breadcrumb-item>
         </el-breadcrumb>
         <br>
-        <el-form ref="role" :model="role" label-width="80px" size="medium">
+        <el-form ref="role" :model="task" label-width="80px" size="medium">
           <el-form-item label="任务名称" prop="name">
-            <el-input v-model="role.name" style="width:280px;"></el-input>
+            <el-input v-model="task.name" style="width:280px;"></el-input>
           </el-form-item>
           <el-form-item label="类型" prop="state">
-             <el-radio-group v-model="role.state">
+             <el-radio-group v-model="task.type">
               <el-radio label="0">执行一次</el-radio>
               <el-radio label="1">循环执行</el-radio>
             </el-radio-group>
           </el-form-item>
           <el-form-item label="时间间隔">
-                <el-input placeholder="请输入间隔时间" v-model="input" class="input-with-select" style="width:280px;">
+                <el-input placeholder="请输入间隔时间" v-model="task.interval" class="input-with-select" style="width:280px;">
                     <el-select v-model="select" slot="append" placeholder="秒">
                     <el-option label="秒" value="1"></el-option>
                     <el-option label="分" value="2"></el-option>
@@ -28,34 +28,23 @@
                 </el-input>
           </el-form-item>
           <el-form-item label="起止时间" prop="state">
-             <el-time-select
-                placeholder="起始时间"
-                v-model="startTime"
-                :picker-options="{
-                start: '08:30',
-                step: '00:15',
-                end: '18:30'
-                }">
-            </el-time-select>
-            <el-time-select
-                placeholder="结束时间"
-                v-model="endTime"
-                :picker-options="{
-                start: '08:30',
-                step: '00:15',
-                end: '18:30',
-                minTime: startTime
-                }">
-            </el-time-select>
+            <el-date-picker
+                v-model="task.beginTime"
+                type="daterange"
+                value-format="timestamp"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期">
+            </el-date-picker>
           </el-form-item>
-          <el-form-item label="执行任务" style="width:600px" prop="description">
-            <el-input v-model="role.name" style="width:280px;"></el-input>
+          <el-form-item label="执行任务" style="width:600px" prop="action">
+            <el-input v-model="task.action" style="width:280px;"></el-input>
           </el-form-item>
           <el-form-item label="备注" style="width:600px" prop="description">
-            <el-input type="textarea" v-model="role.description"></el-input>
+            <el-input type="textarea" v-model="task.description"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button type="primary" @click="onUpdate(task.crontabId)">立即修改</el-button>
             <el-button>取消</el-button>
           </el-form-item>
         </el-form>
@@ -68,30 +57,48 @@
   import axios from '~/plugins/axios.js';
   export default {
     layout: 'main',
+     validate ({ params }) {
+      // Must be a number
+      if(/^\d+$/.test(params.id)){
+        this.id = params.id;
+      }
+      return /^\d+$/.test(params.id)
+    },
+    async asyncData ({ params }) {
+      let {data} = await axios.get(`/admin/task/${params.id}`);
+      return {
+        task: data.data,
+      }
+    },
     data() {
       return {
-        role:{
-          name:'',
-          state:'',
+        task:{
+          name: '',
+          type: 0,
+          interval: 0,
+          beginTime: 0,
+          endTime: 0,
+          action: '',
           description:''
         },
-        startTime: '',
-        endTime: '',
-        select: '',
-        dialogVisible: false
+        select: ''
       }
     },
     methods: {
-      onSubmit() {
-        axios.post('/admin/task',qs.stringify({
-            name: this.role.name,
-            state: this.role.state,
-            description: this.role.description
+      onUpdate(val) {
+        axios.put(`/admin/task/${val}`,qs.stringify({
+            name: this.task.name,
+            type: this.task.type,
+            interval: this.task.interval,
+            beginTime: this.task.beginTime[0],
+            endTime: this.task.beginTime[1],
+            action: this.task.action,
+            description: this.task.description
           })).then(res => {
             //判断是否请求成功
             if(res.data.errorId === 'OK'){
               this.$message({
-                  message: '成功添加角色',
+                  message: '成功编辑定时任务',
                   type: 'success'
                 });
             }
