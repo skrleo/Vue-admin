0a90e6b7f6558a855d83e1e1652ca4d343a2559d
Chenguanghui <13035809409@163.com>
2019-03-28 02:14:32 +0800

    用户详情

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -8,24 +8,52 @@
     <br>
     <div class="banner">
       <el-row :gutter="20">
-        <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4">
+          <img :src="user.headimg" :alt="user.headimg" style="width:100%;height:130px;">
+        </el-col>
+        <el-col :span="4">用户名称：{{user.name}}</el-col>
+        <el-col :span="4">昵称：{{user.nickname}}</el-col>
+        <el-col :span="3">性别：
+          <span v-if="user.sex === 0">保密</span>
+          <span v-else-if="user.sex === 1">男</span>
+          <span v-else>女</span>
+        </el-col>
+        <el-col :span="3">状态：
+          <span v-if="user.status === 0">启用</span>
+          <span v-else>禁用</span>
+        </el-col>
+        <el-col :span="6">邮箱地址：{{user.email}} </el-col>
+        <el-col :span="4">联系方式：{{user.phone}}</el-col>
+        <el-col :span="4">登录次数：{{user.loginNum}} </el-col>
+        <el-col :span="6">最后登录时间：{{user.lastLoginTime | d('yyyy-MM-dd hh:mm:ss')}}</el-col>
+        <el-col :span="2"><el-button type="text">修改密码</el-button></el-col>
       </el-row>
       <el-row :gutter="20">
-        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="16">
+          <el-tabs type="border-card">
+            <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+            <el-tab-pane label="操作历史">操作历史</el-tab-pane>
+            <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+          </el-tabs>
+        </el-col>
+        <el-col :span="8">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>卡片名称</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{'列表内容 ' + o }}
+            </div>
+          </el-card>
+        </el-col>
       </el-row>
     </div>
 </div>
 </template>
 <script>
+  import qs from 'qs';
+  import axios from '~/plugins/axios.js'
   export default {
     layout: 'main',
     validate ({ params }) {
@@ -34,6 +62,12 @@
         this.id = params.id;
       }
       return /^\d+$/.test(params.id)
+    },
+    async asyncData ({ params }) {
+      let {data} = await axios.get(`/admin/user/${params.id}`);
+      return {
+        user: data.data
+      }
     },
     data() {
       return {}
@@ -54,6 +88,8 @@
     }
   }
   .el-col {
+    color: #000;
+    margin-bottom: 12px;
     border-radius: 4px;
   }
   .bg-purple-dark {
