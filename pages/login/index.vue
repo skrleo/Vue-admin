5096259d2999b29c7863a1d0557670c73c2d24e5
Chenguanghui <13035809409@163.com>
2019-03-31 12:56:27 +0800

    fix

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -73,7 +73,7 @@ export default {
         ruleForm: {
           imgVerifySrc:'',
           imgVerify:'',
-          verifyGuid:'123456',
+          verifyGuid: this.createGuid(),
           account:  '',
           password: ''
         },
@@ -162,6 +162,12 @@ export default {
             return false;
           }
         });
+      },
+      createGuid() {
+          return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+              var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+              return v.toString(16);
+          });
       }
     },
 		mounted () {
