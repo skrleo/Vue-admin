539e28de51020d79782a6b08b72a87547b8f9cec
Chenguanghui <13035809409@163.com>
2019-04-06 22:15:53 +0800

    move·

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -176,25 +176,6 @@ export default {
       if (rediretUrl){
         this.redirectURL = rediretUrl
       }
-      var a_index = 0;
-      $("body").click(function(e){
-          var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
-          var $i = $("<span/>").text(a[a_index]);
-          a_index = (a_index + 1) % a.length;
-          var x = e.pageX,y = e.pageY;
-          $i.css({
-              "z-index": 99999,
-              "top": y - 20,
-              "left": x,
-              "position": "absolute",
-              "font-weight": "bold",
-              "color": "#ff6651"
-          });
-          $("body").append($i);
-          $i.animate({"top": y-180,"opacity": 0},1500,function() {
-              $i.remove();
-          });
-      });
 		}
 }
 </script>
