3bef510db77a8001392d2613886d49ccd920aeed
Chenguanghui <13035809409@163.com>
2019-04-07 00:04:56 +0800

    fix

--- store/index.js
+++ store/index.js
@@ -6,7 +6,16 @@ Vue.use(Vuex)
 const store = () => new Vuex.Store({
 
   state: {
-    counter: 0
+    counter: 0,
+    userInfo: {
+      account: '',
+      email: '',
+      headimg: '',
+      phone: '',
+      sex: '',
+      uid: '',
+      username: ''
+    }
   },
   mutations: {
     increment (state) {
