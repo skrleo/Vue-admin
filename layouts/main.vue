3c6454b06ef3f1b63d22f5a9c94ed59186086a4d
Chenguanghui <13035809409@163.com>
2019-10-09 22:54:39 +0800

    rythm.js

--- layouts/main.vue
+++ layouts/main.vue
@@ -303,11 +303,7 @@
     },
     methods: {
       handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => {});
+        done();
       }
     },
     mounted () {
@@ -326,27 +322,51 @@
                 console.log('something wrong!')
             }
         
+        var wsUrl = 'ws://192.168.10.10:52000';
+        var webSocket  = new WebSocket(wsUrl);
+    
+        //实例的对象 onOpen属性
+        webSocket.onopen = function (evt) {
+            //发送数据
+            webSocket.send("hello_zzs");
+            console.log("connected-swoole-success");
+        }
+    
+        //实例化对象  onMessage  接收服务端的数据的函数
+        webSocket.onmessage = function (evt) {
+            console.log("ws-server-return-data:" + evt.data);
+        }
+    
+        //实例化onClose事件
+        webSocket.onclose = function (evt) {
+            console.log("close");
+        }
+    
+        //返回错误的事件
+        webSocket.onerror = function (evt,e) {
+            console.log("error:" + evt.data);
+        }
+
+        var a_index = 0;
+        $("body").click(function(e){
+            var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
+            var $i = $("<span/>").text(a[a_index]);
+            a_index = (a_index + 1) % a.length;
+            var x = e.pageX,y = e.pageY;
+            $i.css({
+                "z-index": 99999,
+                "top": y - 20,
+                "left": x,
+                "position": "absolute",
+                "font-weight": "bold",
+                "color": "#ff6651"
+            });
+            $("body").append($i);
+            $i.animate({"top": y-180,"opacity": 0},1500,function() {
+                $i.remove();
+            });
         });
-      var a_index = 0;
-      $("body").click(function(e){
-          var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
-          var $i = $("<span/>").text(a[a_index]);
-          a_index = (a_index + 1) % a.length;
-          var x = e.pageX,y = e.pageY;
-          $i.css({
-              "z-index": 99999,
-              "top": y - 20,
-              "left": x,
-              "position": "absolute",
-              "font-weight": "bold",
-              "color": "#ff6651"
-          });
-          $("body").append($i);
-          $i.animate({"top": y-180,"opacity": 0},1500,function() {
-              $i.remove();
-          });
-      });
-		}
+    }
   }
 </script>
 <style scoped lang="scss">
